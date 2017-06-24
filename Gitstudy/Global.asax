a47e8905155b69aa1c4847ccabe90dcd708d60d5
Erryl Alvares <erryl1@hotmail.com>
2017-06-24 10:49:45 +0530

    Add project files.

---
+++ Gitstudy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gitstudy.MvcApplication" Language="C#" %>
---
+++ Gitstudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gitstudy.Startup))]
+namespace Gitstudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
